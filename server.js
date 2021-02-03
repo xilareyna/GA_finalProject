70baf50a523b60358334323b356b1bbab297d518
xilareyna <68404722+xilareyna@users.noreply.github.com>
2021-02-03 00:25:18 -0600

    deploy attempt 2

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = require("./backend/app");
 app.use(express.static("build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/build/index.html`));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(`${__dirname}/build/index.html`));
+// });
